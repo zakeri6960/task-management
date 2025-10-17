efc62562c16e670e5d75c7e1311fdf5ea5b00f76
zakeri <m.z.b.6960@gmail.com>
2025-10-17 14:33:15 +0330

    Optimization of responses

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,39 +1,73 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Res, HttpStatus, HttpCode } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
 import { ProjectsStatusEnum } from 'src/Enums/projectEnums';
+import { Response } from 'express';
 
 @Controller('projects')
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
 
   @Post()
-  create(@Body() createProjectDto: CreateProjectDto) {
-    return this.projectsService.create(createProjectDto);
+  @HttpCode(HttpStatus.CREATED)
+  async create(
+    @Body() createProjectDto: CreateProjectDto,
+    @Res() res: Response
+  ) {
+    const result = await this.projectsService.create(createProjectDto);
+    return {
+      statusCode: HttpStatus.CREATED,
+      data: result,
+      message: 'Project creation was successful.'
+    }
   }
 
   @Get()
-  findAll(
+  @HttpCode(HttpStatus.FOUND)
+  async findAll(
     @Query('status') status?: ProjectsStatusEnum,
     @Query('page') page: number = 1,
     @Query('limit') limit: number = 5
   ) {
-    return this.projectsService.findAll(status, page, limit);
+    const result = await this.projectsService.findAll(status, page, limit);
+    return {
+      statusCode: HttpStatus.FOUND,
+      data: result,
+      message: null
+    }
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.projectsService.findOne(id);
+  @HttpCode(HttpStatus.FOUND)
+  async findOne(@Param('id') id: string) {
+    const result = await this.projectsService.findOne(id);
+    return {
+      statusCode: HttpStatus.FOUND,
+      data: result,
+      message: null
+    }
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProjectDto: UpdateProjectDto) {
-    return this.projectsService.update(id, updateProjectDto);
+  @HttpCode(HttpStatus.OK)
+  async update(@Param('id') id: string, @Body() updateProjectDto: UpdateProjectDto) {
+    await this.projectsService.update(id, updateProjectDto);
+    return {
+      statusCode: HttpStatus.OK,
+      data: null,
+      message: 'The project update was successful.'
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.projectsService.remove(id);
+  @HttpCode(HttpStatus.OK)
+  async remove(@Param('id') id: string) {
+    await this.projectsService.remove(id);
+    return {
+      statusCode: HttpStatus.OK,
+      data: null,
+      message: 'Project deletion was successful.'
+    }
   }
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, HttpStatus, HttpCode } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
@@ -9,31 +9,61 @@ export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
-  create(@Body() createTaskDto: CreateTaskDto) {
-    return this.tasksService.create(createTaskDto);
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createTaskDto: CreateTaskDto) {
+    const result = await this.tasksService.create(createTaskDto);
+    return {
+          statusCode: HttpStatus.CREATED,
+          data: result,
+          message: 'Task creation was successful.'
+        }
   }
 
   @Get()
-  findAll(
+  @HttpCode(HttpStatus.FOUND)
+  async findAll(
     @Query('status') status?: TasksStatusEnum,
     @Query('page') page: number = 1,
     @Query('limit') limit: number = 5 
   ) {
-    return this.tasksService.findAll(status, page, limit);
+    const result = await this.tasksService.findAll(status, page, limit);
+    return {
+      statusCode: HttpStatus.FOUND,
+      data: result,
+      message: null
+    }
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.tasksService.findOne(id);
+  @HttpCode(HttpStatus.FOUND)
+  async findOne(@Param('id') id: string) {
+    const result = await this.tasksService.findOne(id);
+    return {
+      statusCode: HttpStatus.FOUND,
+      data: result,
+      message: null
+    }
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
-    return this.tasksService.update(id, updateTaskDto);
+  @HttpCode(HttpStatus.OK)
+  async update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
+    await this.tasksService.update(id, updateTaskDto);
+    return {
+      statusCode: HttpStatus.OK,
+      data: null,
+      message: 'The task update was successful.'
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.tasksService.remove(id);
+  @HttpCode(HttpStatus.OK)
+  async remove(@Param('id') id: string) {
+    await this.tasksService.remove(id);
+    return {
+      statusCode: HttpStatus.OK,
+      data: null,
+      message: 'Project deletion was successful.'
+    }
   }
 }
